4c3cd7525ea5cbdb92e4fda08c929d0dabc3866a
NET Developer <125025933+azimochilov@users.noreply.github.com>
2023-02-12 16:06:38 +0300

    Update User.cs

--- PharmacyEdition.Domain/Entities/User.cs
+++ PharmacyEdition.Domain/Entities/User.cs
@@ -2,7 +2,7 @@
 
 namespace PharmacyEdition.Domain.Entities;
 
-public class User:Auditable
+public class User : Auditable
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
